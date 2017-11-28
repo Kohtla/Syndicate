635d196cf38cbdd55bf093bbd7592b201694047d
Kohtla <MKmoolooo@yandex.ru>
2017-11-28 17:25:41 +0400

    Admin and database
    Admin, database, interface added.

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Theme, Message
 
-# Register your models here.
+admin.site.register(Theme)
+
+
+
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Theme(models.Model):
+    creator = models.CharField(max_length = 250)
+    theme_title = models.CharField(max_length = 1000)
+    theme_image = models.CharField(max_length = 2000)
+    def __str__(self):
+        return self.creator + " - " +self.theme_title
+
+class Message(models.Model):
+    theme  = models.ForeignKey(Theme, on_delete=models.CASCADE)
+    author = models.CharField(max_length = 250)
+    message_text = models.CharField(max_length = 2000)
+    answer_to = models.CharField(max_length = 250)
+    def __str__(self):
+        return self.author + " - " +self.message_text
