6ee64bf42a8ae0a57b0234ce6ce61cca2b7e36a8
Kohtla <MKmoolooo@yandex.ru>
2018-02-01 16:07:41 +0400

    API

--- Syndicate/urls.py
+++ Syndicate/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+from shop import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^forum/', include('forum.urls')),
+    url(r'^shop/', views.ProductList.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+class ProductList(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
